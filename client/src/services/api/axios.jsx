63de4f091d456eb3c0529994c3ab6ba8c9aa9834
anshit-rangra <anshit22rangra@gmail.com>
2026-09-17 12:41:30 +0530

    changing url

--- client/src/services/api/axios.jsx
+++ client/src/services/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:"http://localhost:5173/api",
+    baseURL:"https://complaint-box-delta.vercel.app//api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
