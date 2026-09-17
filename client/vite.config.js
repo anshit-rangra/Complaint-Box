3e43911e38f32db63de535a88fb9c9768ae59020
anshit-rangra <anshit22rangra@gmail.com>
2026-09-17 12:44:52 +0530

    new

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_BACKEND_URI,
+          target: env.VITE_BACKEND_URI || "https://complaint-box-dwov.onrender.com/",
         }
       }
     }
