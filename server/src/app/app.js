8699e285fb745fef8c7e562755e69538855a5475
anshit-rangra <anshit22rangra@gmail.com>
2026-09-20 19:52:56 +0530

    Production error fix

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,9 +1,16 @@
 import express from "express"
 import complaintRoutes from "../routes/complaint.routes.js"
+import cors from "cors";
+
 
 const app = express();
 
 app.use(express.json())
+
+
+app.use(cors({
+  origin: "https://your-frontend.vercel.app"
+}));
 
 app.use("/api/complaint", complaintRoutes)
 
