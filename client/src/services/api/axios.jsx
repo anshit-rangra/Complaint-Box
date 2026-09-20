62919603c6f432f3e9920019e8fc00cf14c00b72
anshit-rangra <anshit22rangra@gmail.com>
2026-09-20 19:55:06 +0530

    fix

--- client/src/services/api/axios.jsx
+++ client/src/services/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_FRONTEND_URI}/api`,
+    baseURL: `${import.meta.env.VITE_API_URI}/api`,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
